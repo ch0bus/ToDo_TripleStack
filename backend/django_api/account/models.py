9ee1c989aaecec90fad98c22850de713e40fbe23
chobus <ch0bus>
2026-09-04 15:27:37 +0300

    feat: добавить аутентификацию

---
+++ backend/django_api/account/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
